1004d409dd75c51fd43fc5a57f5e4ce6058684ee
mohammadliazul <liazulislam0@gmail.com>
2023-02-08 01:59:32 +0600

    put api added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 
 const cors = require("cors");
 
+// middle ware 
 app.use(cors());
 app.use(express.json());
 
@@ -43,6 +44,16 @@ const run = async () => {
       const result = await productCollection.deleteOne({ _id: ObjectId(id) });
       res.send(result);
     });
+
+    app.put('/product/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const updatedProduct = req.body;
+      const updateDoc = { $set: updatedProduct};
+      const options = { upsert: true };
+      const result = await productCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+  });
 
   } finally {
   }
